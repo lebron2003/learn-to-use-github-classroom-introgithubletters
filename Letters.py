320d22bc24eac772d9acf504817970bb9381bcfa
manga9158 <manga9158@wrdsb.ca>
2020-02-19 12:34:45 -0500

    Added print

--- Letters.py
+++
@@ -1,2 +0,0 @@
-print("Hello")
-print("What is your name")
---
+++ lol.py
@@ -0,0 +1,6 @@
+import pygame
+import turtle
+
+surface = pygame.Surface((100, 100)) # setup a very basic surface - you may overload or change for your version or use this.
+surface = pygame.Surface((100, 100), pygame.SRCALPHA) # transparent one, changed my mind...
+image = pygame.image.load('LetterZ.png')
